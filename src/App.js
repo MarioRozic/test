a176c6b3bb96b0be085f3f606d738bdb469623be
MarioRozic <rozic.mario@hotmail.com>
2020-11-27 13:55:19 +0100

    :sparkles: Init mario 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Mario</p>
+        <p>Mario aksjhdjkashkj</p>
       </header>
     </div>
   );
